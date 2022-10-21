77c761d000ef00ad097d26eece5fd53f3ce9cdd1
Lorvaill <96297088+Lorvaill@users.noreply.github.com>
2022-10-21 11:58:58 +0200

    Maj Templates

--- src/main/java/viikingit/emusic/controller/MainController.java
+++ src/main/java/viikingit/emusic/controller/MainController.java
@@ -1,6 +1,7 @@
 package viikingit.emusic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +10,11 @@ public class MainController {
 	@RequestMapping
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("cours")
+	public String AllerACours() {
+		return "cours/list_cours";
 	}
 
 }
