8a5e06f6858013f87236a68350834c20f4936b7a
Lorvaill <96297088+Lorvaill@users.noreply.github.com>
2022-11-03 17:16:42 +0100

    Créer un cours
    paramètrage d'un cours afin de le créer par la suite

---
+++ src/main/java/viikingit/emusic/controller/CoursController.java
@@ -0,0 +1,20 @@
+package viikingit.emusic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import viikingit.emusic.models.Cours;
+
+@Controller
+public class CoursController {
+	
+	@GetMapping("newCours")
+	public String newCours(ModelMap model) {
+		model.addAttribute("orga", new Cours());
+		return "cours/formNewCours";
+	}
+	
+	
+	
+}
