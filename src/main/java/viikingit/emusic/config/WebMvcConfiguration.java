2680d58a0ed484849fa7a07720aa7ae6efd7a989
Sabri <90392579+Sabrrii@users.noreply.github.com>
2022-11-06 19:43:30 +0100

    Maj vacance

---
+++ src/main/java/viikingit/emusic/config/WebMvcConfiguration.java
@@ -0,0 +1,19 @@
+package viikingit.emusic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+ 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+    	//registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logout").setViewName("logout");
+        registry.addViewController("/403").setViewName("/errors/403");
+        registry.addViewController("/404").setViewName("/errors/404");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
