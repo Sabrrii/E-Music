113ee0394f6015d04b75ac9d67391011ace2fe2e
adrienlg <90392648+Comcombre@users.noreply.github.com>
2022-11-16 09:28:23 +0100

    v1.1 online

--- src/main/java/viikingit/emusic/controller/CartesControlleur.java
+++ src/main/java/viikingit/emusic/controller/CartesControlleur.java
@@ -1,14 +1,11 @@
 package viikingit.emusic.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -27,22 +24,23 @@ public class CartesControlleur {
 	private IParentRepository parentRepo;
 
 	@GetMapping("myCard/{id}")
-	public String formCarteLog(ModelMap model,@AuthenticationPrincipal Parent authUSer) {
-		if(authUSer.getCarte()== null) {
-			Cartes cb=new Cartes();
+	public String formCarteLog(ModelMap model, @AuthenticationPrincipal Parent authUSer) {
+		if (authUSer.getCarte() == null) {
+			Cartes cb = new Cartes();
 			cb.setUser(authUSer);
 			authUSer.setCarte(cb);
 			carteRepo.save(cb);
 		}
-		
-		model.put("userCo",authUSer);
-		return "/user/FormCartes";
+
+		model.put("userCo", authUSer);
+		return "user/FormCartes";
 	}
 
 	@PostMapping("myCard/edit")
-	public RedirectView addCarte(ModelMap model,@AuthenticationPrincipal Parent authUSer, @ModelAttribute Cartes carte) {
+	public RedirectView addCarte(ModelMap model, @AuthenticationPrincipal Parent authUSer,
+			@ModelAttribute Cartes carte) {
 		int toChange = authUSer.getCarte().getId();
-		Cartes toAdd =carte;
+		Cartes toAdd = carte;
 		toAdd.setId(toChange);
 		toAdd.setUser(authUSer);
 		authUSer.setCarte(toAdd);
