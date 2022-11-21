a6296a95431c8127b14914ba9cf85a2f3684815a
Lorvaill <96297088+Lorvaill@users.noreply.github.com>
2022-11-21 09:48:07 +0100

    Supprimer un cours

--- src/main/java/viikingit/emusic/controller/CoursController.java
+++ src/main/java/viikingit/emusic/controller/CoursController.java
@@ -19,7 +19,6 @@ import viikingit.emusic.models.TypeCours;
 import viikingit.emusic.repository.ICoursRepository;
 import viikingit.emusic.repository.IInstrumentsRepository;
 import viikingit.emusic.repository.ITypeCoursReposiroty;
-import viikingit.emusic.ui.UIMessage;
 
 @Controller
 public class CoursController {
@@ -69,46 +68,34 @@ public class CoursController {
 		return "cours/list_instruments";
 	}
 
-	/*
-	 * Optional<Parent> opt = parRepo.findById(authUser.getId()); opt.ifPresent(orga
-	 * -> model.put("parent",orga)); model.put("userCo", authUser);
-	 */
-
 	@GetMapping("editCours/{id}")
-	public String formEditCours(ModelMap model, @AuthenticationPrincipal Parent authUser, @PathVariable int id) {
+	public String formEditCours(ModelMap model, @PathVariable int id) {
 		Optional<Cours> cour = courRepo.findById(id);
-		cour.ifPresent(orga -> model.put("cours", orga));
+		cour.ifPresent(cours -> model.put("cours", cours));
 		model.put("type_cours", typecours.findAll());
-		model.put("userCo", authUser);
 		return "cours/formEditCours";
 	}
 
-	@PostMapping("editCours/{id}")
-	public String edited(ModelMap model, @PathVariable int id) {
-		courRepo.findById(id).ifPresent(cours -> model.put("cour", cours));
-		return "/listCours";
+//	@GetMapping("editCours/{id}")
+//	public String edited(ModelMap model, @PathVariable int id) {
+//		courRepo.findById(id).ifPresent(cour -> model.put("cour", cour));
+//		return "/cours/formEditCours";
 
-		// Cours toSave = courRepo.findById(id).get();
-		// toSave.setLibelle(cours.getLibelle());
-		// toSave.setAgeMaxi(cours.getAgeMaxi());
-		// toSave.setAgeMini(cours.getAgeMini());
-		// toSave.setNbPlaces(cours.getNbPlaces());
-		// toSave.setInstrument(cours.getInstrument());
-		// toSave.setTypeCour(cours.getTypeCour());
-		// courRepo.save(toSave);
-		// return new RedirectView("/listCours");
-	}
+	// Cours toSave = courRepo.findById(id).get();
+	// toSave.setLibelle(cours.getLibelle());
+	// toSave.setAgeMaxi(cours.getAgeMaxi());
+	// toSave.setAgeMini(cours.getAgeMini());
+	// toSave.setNbPlaces(cours.getNbPlaces());
+	// toSave.setInstrument(cours.getInstrument());
+	// toSave.setTypeCour(cours.getTypeCour());
+	// courRepo.save(toSave);
+	// return new RedirectView("/listCours");
+	// }
 
 	@GetMapping("delete/{id}")
 	public RedirectView deleteAction(@PathVariable int id, RedirectAttributes attrs) {
-		Optional<Cours> opt = courRepo.findById(id);
-		if (opt.isPresent()) {
-			attrs.addFlashAttribute("msg",
-					UIMessage.errorConfirmation("Suppression",
-							"Voulez-vous supprimer <strong>`" + opt.get() + "`</strong>", "/cours/delete/force/" + id,
-							"/cours"));
-		}
+		courRepo.findById(id);
+		courRepo.deleteById(id);
 		return new RedirectView("/listCours");
-
 	}
 }
