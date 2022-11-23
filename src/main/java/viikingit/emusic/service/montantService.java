f9e4c92ccceffbd9ae9e0ad21451323cdae1612f
adrienlg <90392648+Comcombre@users.noreply.github.com>
2022-11-23 10:56:07 +0100

    Update montantService.java

--- src/main/java/viikingit/emusic/service/montantService.java
+++ src/main/java/viikingit/emusic/service/montantService.java
@@ -1,7 +1,11 @@
 package viikingit.emusic.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import viikingit.emusic.models.Parent;
+import viikingit.emusic.models.TypeCours;
 import viikingit.emusic.repository.IParentRepository;
 import viikingit.emusic.repository.ITypeCoursReposiroty;
 
@@ -14,8 +18,34 @@ public class montantService {
 
 	// services pour un montant d'un cours
 
-	public int determinerMontant() {
-		ParentRepo.findAll();
-
+	@SuppressWarnings("unlikely-arg-type")
+	public int determinerMontant(int id) {
+		Optional<TypeCours> types = typeCoursRepo.findById(id);
+		Optional<Parent> parentQ = ParentRepo.findById(id);
+		int montant = 0;
+		int Quotient = parentQ.get().getQuotientFamilial();
+		if (Quotient < 0) {
+			montant = 417;
+		} else if (Quotient >= 0 && Quotient <= 250) {
+			montant = 60;
+		} else if (Quotient >= 251 && Quotient <= 425) {
+			montant = 96;
+		} else if (Quotient >= 426 && Quotient <= 680) {
+			montant = 126;
+		} else if (Quotient >= 681 && Quotient <= 935) {
+			montant = 192;
+		} else if (Quotient >= 936 && Quotient <= 1800) {
+			montant = 282;
+		} else if (Quotient >= 1801) {
+			montant = 330;
+		}
+		if (types.equals("Collectif")) {
+			if (Quotient < 0) {
+				montant = (montant + 1) / 2;
+			} else {
+				montant = montant / 2;
+			}
+		}
+		return montant;
 	}
 }
