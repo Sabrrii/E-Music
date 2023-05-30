84d181814c8df73def8c28a504bf234a06c73f86
Sabri <90392579+Sabrrii@users.noreply.github.com>
2023-05-30 16:36:33 +0200

    ajout pojo

---
+++ src/main/java/viikingit/emusic/config/AppConfig.java
@@ -0,0 +1,10 @@
+package viikingit.emusic.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("io.github.jeemv.springboot.vuejs")
+public class AppConfig {
+
+}
---
+++ src/main/java/viikingit/emusic/pojo/ActiveUser.java
@@ -0,0 +1,19 @@
+package viikingit.emusic.pojo;
+
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class ActiveUser {
+    @NonNull
+    private Authentication authentication;
+
+    public boolean isConnected() {
+        return !(authentication instanceof AnonymousAuthenticationToken);
+    }
+}
+
